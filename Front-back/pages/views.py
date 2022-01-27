9e2d9b29dbe0b46770be48b2f7ffb5b1051ca68b
Philippe Garandy <philippegarandy@gmail.com>
2022-01-27 17:24:49 -0300

    set up profile user

--- Front-back/pages/views.py
+++ Front-back/pages/views.py
@@ -1,3 +1,4 @@
+import profile
 import django.contrib.staticfiles
 from django.forms import forms 
 from django.views.generic import TemplateView
@@ -7,6 +8,7 @@ from django.http import HttpRequest, HttpResponse, request
 from django.http import HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
 from users.models import User
+from django.shortcuts import get_object_or_404
 
 class HomePageView(TemplateView):
     template_name = "home.html"
@@ -15,12 +17,18 @@ def Fillprofileview(request):
     if request.method == 'POST':
 
         # Create a form instance and populate it with data from the request (binding):
+        myuser = get_object_or_404(User, pk=request.user.id)
         form = FillProfileForms(request.POST)
-        company_name = forms.cleaned_data['company_name']
+        
         # Check if the form is valid:
         if form.is_valid():
-
-            return HttpResponseRedirect('/thanks/')
+            myuser.company_name = form.cleaned_data['company_name']
+            myuser.organic_numb_certification = form.cleaned_data['organic_numb_certification']
+            myuser.profile_set = True
+            myuser.save()
+            print(myuser.profile_set)
+            print(myuser.username)
+            return HttpResponseRedirect('/showprofile/')
     # If this is a GET (or any other method) create the default form.
     else:
         form = FillProfileForms()
--- Front-back/transaction/apps.py
+++ Front-back/transaction/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class TransationConfig(AppConfig):
+class TransactionConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'transation'
+    name = 'transaction'
